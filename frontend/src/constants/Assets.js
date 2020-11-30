8db59fd6592986518bb7a353830fb71e75947156
Sam <super8989@gmail.com>
2020-11-29 20:56:43 -0500

    Exported all asset files

---
+++ frontend/src/constants/Assets.js
@@ -0,0 +1,25 @@
+export default {
+	cards: {
+		amexDisabled: require('../assets/cards/amex-disabled.svg'),
+		amexEnabled: require('../assets/cards/amex-enabled.svg'),
+		discoverDisabled: require('../assets/cards/discover-disabled.svg'),
+		discoverEnabled: require('../assets/cards/discover-enabled.svg'),
+		masterDisabled: require('../assets/cards/master-disabled.svg'),
+		masterEnabled: require('../assets/cards/master-enabled.svg'),
+		visaDisabled: require('../assets/cards/visa-disabled.svg'),
+		visaEnabled: require('../assets/cards/visa-enabled.svg'),
+	},
+	icons: {
+		alertTriangle: require('../assets/icons/alert-triangle.svg'),
+		arrowLeft: require('../assets/icons/arrow-left.svg'),
+		checkCircle: require('../assets/icons/check-circle.svg'),
+		chevronDown: require('../assets/icons/chevron-down.svg'),
+		creditCard: require('../assets/icons/credit-card.svg'),
+		dollarSign: require('../assets/icons/dollar-sign.svg'),
+		helpCircle: require('../assets/icons/help-circle.svg'),
+		user: require('../assets/icons/user.svg'),
+	},
+	images: {
+		chair: require('../assets/images/chair.png'),
+	},
+};
