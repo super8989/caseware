54d1857441b360c7d792ccb94d20e45451202d32
Sam <super8989@gmail.com>
2020-11-29 21:09:00 -0500

    Exported all components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as PrimaryButton } from './PrimaryButton';
+export { default as SecondaryButton } from './SecondaryButton';
+export { default as TextField } from './TextField';
+export { default as Selection } from './Selection';
