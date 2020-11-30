761ffaa133c3f8372719dd59e20174fb2e478cd8
Sam <super8989@gmail.com>
2020-11-29 20:58:10 -0500

    Created months arrays

---
+++ frontend/src/constants/months.js
@@ -0,0 +1,14 @@
+export default [
+	{ name: '01 - January', value: 1 },
+	{ name: '02 - February', value: 2 },
+	{ name: '03 - March', value: 3 },
+	{ name: '04 - April', value: 4 },
+	{ name: '05 - May', value: 5 },
+	{ name: '06 - June', value: 6 },
+	{ name: '07 - July', value: 7 },
+	{ name: '08 - August', value: 8 },
+	{ name: '09 - September', value: 9 },
+	{ name: '10 - October', value: 10 },
+	{ name: '11 - November', value: 11 },
+	{ name: '12 - December', value: 12 },
+];
