8f5a97b8fd6b7e81322427cca7b5a63baefbd677
Sam <super8989@gmail.com>
2020-11-29 21:07:12 -0500

    Created Payment view component

---
+++ frontend/src/views/Payment/index.js
@@ -0,0 +1,199 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+	Header,
+	PrimaryButton,
+	SecondaryButton,
+	TextField,
+	Selection,
+} from '../../components';
+import { Assets } from '../../constants';
+import './style.scss';
+
+function Payment() {
+	const history = useHistory();
+	const [cardInfo, setCardInfo] = useState({
+		number: null,
+		name: '',
+		month: null,
+		year: null,
+		cvv: null,
+	});
+
+	const [error, setError] = useState(false);
+
+	// const checkCardType = (digit) => {
+	// 	console.log(digit);
+	// 	const firstDigit = digit.charAt(0);
+	// 	switch (firstDigit) {
+	// 		case '3':
+	// 			setCardType('amex');
+	// 			break;
+	// 		case '4':
+	// 			setCardType('visa');
+	// 			break;
+	// 		case '5':
+	// 			setCardType('master');
+	// 			break;
+	// 		case '6':
+	// 			setCardType('discover');
+	// 			break;
+	// 		default:
+	// 			setError(true);
+	// 	}
+	// };
+
+	const handleCardNumberInput = (number) => {
+		setCardInfo({ ...cardInfo, number: Number(number.target.value) });
+	};
+
+	const handleCardNameInput = (name) => {
+		setCardInfo({ ...cardInfo, name: name.target.value });
+	};
+
+	const handleMonthInput = (month) => {
+		setCardInfo({ ...cardInfo, month: Number(month.target.value) });
+	};
+
+	const handleYearInput = (year) => {
+		setCardInfo({ ...cardInfo, year: Number(year.target.value) });
+	};
+
+	const handleCvvInput = (digit) => {
+		setCardInfo({ ...cardInfo, cvv: Number(digit.target.value) });
+	};
+
+	const isFormComplete = () => {
+		if (
+			cardInfo.number === null ||
+			cardInfo.number === 0 ||
+			cardInfo.name === '' ||
+			cardInfo.month === null ||
+			cardInfo.month === 0 ||
+			cardInfo.year === null ||
+			cardInfo.year === 0 ||
+			cardInfo.cvv === null ||
+			cardInfo.cvv === 0
+		) {
+			setError(true);
+			return false;
+		}
+
+		setError(false);
+		return true;
+	};
+
+	const handleSubmit = async () => {
+		try {
+			const body = { cardInfo };
+
+			const response = await fetch('http://localhost:5000', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(body),
+			}).then((result) => {
+				console.log('result', result);
+			});
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	return (
+		<div className='Payment'>
+			<Header>
+				<img
+					className='dollar-sign'
+					src={Assets.icons.dollarSign.default}
+					alt='Dollar Sign'
+				/>
+				Complete your payment
+			</Header>
+			<div className='Payment__left'>
+				<div className='Payment__left--amount'>
+					<span className='Payment__left--amount--text'>Amount Due</span>
+					<span className='Payment__left--amount--price'>$249.00</span>
+				</div>
+				<div className='Payment__left--order'>
+					<span className='Payment__left--order--text'>Order Number</span>
+					<span className='Payment__left--order--number'>1524851</span>
+				</div>
+				<div className='Payment__left--back'>
+					<SecondaryButton onClick={() => history.push('/')}>
+						<img
+							className='arrow-left'
+							src={Assets.icons.arrowLeft.default}
+							alt='Left Arrow'
+						/>
+						<span className='Payment__left--back--text'>Back</span>
+					</SecondaryButton>
+				</div>
+			</div>
+			<div className='Payment__right'>
+				<div className='Payment__right--credit-cards'>
+					<img
+						className='visa'
+						src={Assets.cards.visaDisabled.default}
+						alt='VISA'
+					/>
+					<img
+						className='discover'
+						src={Assets.cards.discoverDisabled.default}
+						alt='Discover'
+					/>
+					<img
+						className='amex'
+						src={Assets.cards.amexDisabled.default}
+						alt='AMEX'
+					/>
+					<img
+						className='master'
+						src={Assets.cards.masterDisabled.default}
+						alt='Master Card'
+					/>
+				</div>
+				<div className='Payment__right--card-info'>
+					<div className='Payment__right--card-info--number'>
+						<TextField
+							label='Card Number'
+							placeholder='#### #### #### ####'
+							type='tel'
+							maxLength='16'
+							icon={Assets.icons.creditCard.default}
+							onChange={handleCardNumberInput}
+						/>
+					</div>
+					<div className='Payment__right--card-info--name'>
+						<TextField
+							label='Cardholder Name'
+							placeholder='Name on the card'
+							type='text'
+							icon={Assets.icons.user.default}
+							onChange={handleCardNameInput}
+						/>
+					</div>
+					<div className='Payment__right--card-info--expiry'>
+						<Selection
+							label='Expiry Date'
+							onMonthChange={handleMonthInput}
+							onYearChange={handleYearInput}
+						/>
+					</div>
+					<div className='Payment__right--card-info--cvv'>
+						<TextField
+							label='CVV'
+							type='tel'
+							maxLength='4'
+							onChange={handleCvvInput}
+						/>
+					</div>
+				</div>
+				<div className='Payment__submit'>
+					<PrimaryButton onClick={handleSubmit}>Submit</PrimaryButton>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Payment;
